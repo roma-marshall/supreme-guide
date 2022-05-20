601e56c2a0b7a4cb12b94848b6c834302f133848
roma_marshall <roma_marshall@outlook.com>
2022-05-20 15:03:35 +0200

    min max added

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	bubbleArray := []int{1, 3, 9, -5, 0}
-
+	bubbleArray := []int{1, 3, 9, -5, 0, 2, 77, 11}
+	max := 0
+	min := 0
 	for i := 0; i < len(bubbleArray); i++ {
-		fmt.Println(bubbleArray[i])
+		fmt.Print(bubbleArray[i], " ")
+		if bubbleArray[i] > max {
+			max = bubbleArray[i]
+		}
+		if bubbleArray[i] < min {
+			min = bubbleArray[i]
+		}
 	}
+	fmt.Println("\nmax: ", max)
+	fmt.Println("min: ", min)
 }
