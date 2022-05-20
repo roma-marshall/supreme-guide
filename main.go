975b9b4c990b9b040753119083d00eb5f07bd63b
roma_marshall <roma_marshall@outlook.com>
2022-05-20 15:14:01 +0200

    bubbleSort

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	bubbleArray := []int{1, 3, 9, -5, 0, 2, 77, 11}
-	max := 0
 	min := 0
-	for i := 0; i < len(bubbleArray); i++ {
-		fmt.Print(bubbleArray[i], " ")
-		if bubbleArray[i] > max {
-			max = bubbleArray[i]
-		}
+	max := 0
+	temp := 0
+	for i := len(bubbleArray) - 1; i > 0; i-- {
 		if bubbleArray[i] < min {
 			min = bubbleArray[i]
 		}
+		if bubbleArray[i] > max {
+			max = bubbleArray[i]
+		}
+		for j := 0; j < i; j++ {
+			if bubbleArray[j] > bubbleArray[j+1] {
+				temp = bubbleArray[j]
+				bubbleArray[j] = bubbleArray[j+1]
+				bubbleArray[j+1] = temp
+			}
+		}
 	}
+	for i := 0; i < len(bubbleArray); i++ {
+		fmt.Print(bubbleArray[i], " ")
+	}
+
 	fmt.Println("\nmax: ", max)
 	fmt.Println("min: ", min)
+}
+
+func Max(bubbleArray []int) {
+	panic("unimplemented")
 }
